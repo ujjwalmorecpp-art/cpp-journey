633656bf0d1957221a86b189e354b8914937efb6
ujjwal.cpp <ujjwal.more.cpp@gmail.com>
2026-08-15 01:03:46 +0530

    WEEK 2: Day 6 - revision 133 COMPLETE: reverseNum from memory (all 5 done)

---
+++ MONTH 2/WEEK 2/revision_reverseNum.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int reverse(int a ){
+
+    int rev = 0;
+    int digit;
+
+    while(a>0){
+        digit = a %10;
+        rev = rev*10 + digit;
+        a = a / 10;
+    }
+    return rev;
+}
+
+int main(){
+
+    int a;
+    cin>>a;
+
+    cout<<reverse(a);
+
+
+    return 0;
+}
